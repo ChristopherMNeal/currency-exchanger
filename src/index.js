bce07758f2568c29d7d27846ba605389cc8a1145
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 13:41:57 -0800

    WIP Submit base currency button for index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './js/exchange-rate-service';
 import CodesService from './js/codes-service';
+import calculateExchange from './js/calculate-exchange';
 
-function getElements(response) {
+function getElements(response, amount) {
   if(response.result === "success") {
-    $("#output-form").text(calculateExchange(response));
+    const rate = response.conversion_rates.baseCurrency;
   } else {
     $(".showErrors").text(`An error occured: ${response}`);
   }
@@ -15,7 +16,7 @@ function getElements(response) {
 
 async function makeApiCall(baseCurrency) {
   const response = await ExchangeRateService.getRate(baseCurrency);
-  getElements(response);
+  // sessionStorage.setItem(response);
 }
 
 async function getMenu() {
@@ -31,7 +32,7 @@ function menuLoop(response) {
   const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","EUR"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
   for (let i = 0; i < mostCommonCurrencies.length; i++) {
     let currencyName = (mostCommonCurrencies[i][1]);
-    let key = (response.supported_codes[i][0]);
+    let key = (mostCommonCurrencies[i][0]);
     let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
     $("#common-base-currencies").append(menuItem);
     $("#common-output-currencies").append(menuItem);
@@ -45,51 +46,22 @@ function menuLoop(response) {
   }
 }
 
-
 getMenu();
 
 $("#base-currency-submit").click(function() {
-  let baseCurrency = $("#baseCurrencyMenu").val();
+  let baseCurrency = "";
+  if ($("#common-base-currencies")) {
+    baseCurrency = $("#common-base-currencies").val();  
+  } else if ($("#all-base-currencies")) {
+    baseCurrency = $("#all-base-currencies").val();
+    console.log(baseCurrency);
+  } else {
+    $(".outputForm").text("Please choose a base currency");
+  }
+  console.log(baseCurrency);
+  $(".base-currency").text(baseCurrency);
   makeApiCall(baseCurrency);
 });
-
-// SETS DON'T WORK LIKE THIS AND I WASTED 2 HOURS TRYING TO MAKE THEM DO IT!
-/*
-async function getMenu() {
-  const response = await CodesService.getCodes();
-  if (response.result === "success") {
-    const menuSet = menuSetLoop(response);
-    menuLoop(menuSet);
-  } else {
-    $(".showErrors").text(`An error occured: ${response}`);
-
-  }
-}
-
-function menuSetLoop(response) {
-
-  let menuSet = new Set();
-  const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","EUR"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
-  for (let i = 0; i < mostCommonCurrencies.length; i++) {
-    let currencyName = (mostCommonCurrencies[i][1]);
-    let key = (mostCommonCurrencies[i][0]);
-    menuSet.add([key, `${currencyName} (${key})`]);
-  }
-  for (let i = 0; i < response.supported_codes.length; i++) {
-    let currencyName = (response.supported_codes[i][1]);
-    let key = (response.supported_codes[i][0]);
-    menuSet.add([key, `${currencyName} (${key})`]);
-  }
-  return menuSet;
-}
-
-function menuLoop(menuSet) {
-  menuSet.forEach((key) => {
-    const name = key[1];
-    const value = key[0];
-    const menuItem = `<option value="${value}">${name}</option>`;
-    $("#base-currencies").append(`${menuItem}`);
-  });
-}
-
-*/
+$("#convert").click(function() {
+  let amount = $("#amount-input").val();
+});
