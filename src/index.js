0daccf1788e87a2c22079becfa8447f74eab3ac3
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 15:59:30 -0800

    Add functionality to convert currency both ways to index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './js/exchange-rate-service';
 import CodesService from './js/codes-service';
-import calculateExchange from './js/calculate-exchange';
 
 function getElements(response) {
-  const amount = sessionStorage.getItem("amount");
   if(response.result === "success") {
     const outputCurrency = sessionStorage.getItem("output");
     const rate = response.conversion_rates[outputCurrency];
-    const answer = calculateExchange(amount, rate);
-    $("#show-amount").text(answer);
+    sessionStorage.setItem("rate", rate);
   } else {
     $(".show-errors").text(`An error occured: ${response}`);
   }
+}
+
+function makeCalculation() {
+  const amount = sessionStorage.getItem("amountIn");
+  const rate = sessionStorage.getItem("rate");
+  const answer = parseFloat((rate*amount).toFixed(2));
+  $("#amount-output").val(answer);
+}
+
+function makeReverseCalculation() {
+  const amount = sessionStorage.getItem("amountOut");
+  const rate = sessionStorage.getItem("rate");
+  const answer = parseFloat((amount/rate).toFixed(2));
+  $("#amount-input").val(answer);
 }
 
 async function makeApiCall(baseCurrency) {
@@ -54,29 +65,54 @@ getMenu();
 
 $("#base-currency-submit").click(function() {
   let baseCurrency = "";
-  if ($("#common-base-currencies").val()) {
+  if ($("#common-base-currencies").val() && $("#all-base-currencies").val()) {
+    $("#currency-select-error").text("Please choose from only one menu.");
+    return false;
+  } else if ($("#common-base-currencies").val()) {
     baseCurrency = $("#common-base-currencies").val();  
   } else if ($("#all-base-currencies").val()) {
     baseCurrency = $("#all-base-currencies").val();
   } else {
-    $("#base-currency-error").text("Please choose a base currency");
+    $("#currency-select-error").text("Please choose a base currency.");
+    return false;
   }
   $(".base-currency").text(baseCurrency);
   sessionStorage.setItem("base", baseCurrency);
+  $("#currency-select-error").text("");
+  $("#output-currency-form").slideDown();
+  $("#base-currency-input").slideUp();
 });
+
 $("#convert").click(function() {
-  let amount = $("#amount-input").val();
-  sessionStorage.setItem("amount", amount);
   let outputCurrency = "";
-  if ($("#common-output-currencies").val()) {
+  if ($("#common-output-currencies").val() && $("#all-output-currencies").val()) {
+    $("#currency-select-error").text("Please choose from only one menu.");
+    return false;
+  } else if ($("#common-output-currencies").val()) {
     outputCurrency = $("#common-output-currencies").val();  
   } else if ($("#all-output-currencies").val()) {
     outputCurrency = $("#all-output-currencies").val();
   } else {
-    $("#output-currency-error").text("Please choose an output currency");
+    $("#currency-select-error").text("Please choose an output currency.");
+    return false;
   }
   $(".output-currency").text(outputCurrency);
   sessionStorage.setItem("output", outputCurrency);
   let baseCurrency = sessionStorage.getItem("base");
   makeApiCall(baseCurrency);
+  $("#currency-select-error").text("");
+  $("#amount-input-form").slideDown();
+  $("#output-currency-form").slideUp();
+});
+
+$("#amount-input").change(function() {
+  let amount = $("#amount-input").val();
+  sessionStorage.setItem("amountIn", amount);
+  makeCalculation();
+});
+
+$("#amount-output").change(function() {
+  let amount = $("#amount-output").val();
+  sessionStorage.setItem("amountOut", amount);
+  makeReverseCalculation();
 });
