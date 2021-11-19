1156695c3c0a2ac5c416bc8d1d6005886deaaab5
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 10:49:39 -0800

    Add UI logic for getMenu()

--- src/index.js
+++ src/index.js
@@ -19,11 +19,66 @@ async function makeApiCall(baseCurrency) {
 }
 
 async function getMenu() {
-  const response - await CodesService.getCodes();
+  const response = await CodesService.getCodes();
   if (response.result === "success") {
-    menuLoop(response);
+    const menuSet = menuSetLoop(response);
+    menuLoop(menuSet);
   } else {
     $(".showErrors").text(`An error occured: ${response}`);
 
   }
 }
+
+function menuSetLoop(response) {
+  let menuSet = new Set();
+  // menuSet.add(["USD","United States Dollar"]);
+  // menuSet.add(["EUR","Euro"]);
+  // menuSet.add(["JPY","Japanese Yen"]);
+  // menuSet.add(["GBP","Pound Sterling"]);
+  // menuSet.add(["AUD","Australian Dollar"]);
+  // menuSet.add(["CAD","Canadian Dollar"]);
+  // menuSet.add(["CHF","Swiss Franc"]);
+  // menuSet.add(["CNY","Chinese Renminbi"]);
+  // menuSet.add(["MXN","Mexican Peso"]);
+  // menuSet.add(["NZD", "New Zealand Dollar"]);
+  // menuSet.add(["SGD", "Singapore Dollar"]);
+  // menuSet.add(["SEK","Swedish Krona"]);
+  // menuSet.add(["South Korean Won"]);
+  // menuSet.add(["TRY","Turkish Lira"]);
+  // menuSet.add(["INR","Indian Rupee"]);
+  // menuSet.add(["BRL","Brazilian Real"]);
+  // menuSet.add(["South African Rand"]);
+  // menuSet.add(["DKK","Danish Krone"]);
+  // menuSet.add(["TWD","New Taiwan Dollar"]);
+  // menuSet.add(["MYR","Malaysian Ringgit"]);
+
+  const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","Euro"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
+  for (let i = 0; i < mostCommonCurrencies.length; i++) {
+    let currencyName = (mostCommonCurrencies[i][1]);
+    let key = (mostCommonCurrencies[i][0]);
+    menuSet.add([key, `${currencyName} (${key})`]);
+  }
+  for (let i = 0; i < response.length; i++) {
+    let currencyName = (response.supported_codes[i][1]);
+    let key = (response.supported_codes[i][0]);
+    menuSet.add([key, `${currencyName} (${key})`]);
+  }
+  return menuSet;
+}
+
+function menuLoop(menuSet) {
+  menuSetLoop(menuSet);
+  menuSet.forEach((key) => {
+    const name = key[1];
+    const value = key[0];
+    const menuItem = `<option value="${value}">${name}</option>`;
+    $("#currencies").append(`${menuItem}`);
+  });
+}
+
+getMenu();
+
+$("#base-currency-submit").click(function() {
+  let baseCurrency = $("#baseCurrencyMenu").val();
+  makeApiCall(baseCurrency);
+})
