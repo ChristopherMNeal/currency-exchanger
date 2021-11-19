a310da525266c1033ad3d2ca17f388ab489e7f48
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 10:01:33 -0800

    Add async functions to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from './js/exchange-rate-service';
+import CodesService from './js/codes-service';
+
+function getElements(response) {
+  if(response.result === "success") {
+    $("#output-form").text(convertFunction(response))
+  } else {
+    $(".showErrors").text(`An error occured: ${response}`);
+  }
+}
+
+async function makeApiCall(baseCurrency) {
+  const response = await ExchangeRateService.getRate(baseCurrency);
+  getElements(response);
+}
+
+async function getMenu() {
+  const response - await CodesService.getCodes();
+  if (response.result === "success") {
+    menuLoop(response);
+  } else {
+    $(".showErrors").text(`An error occured: ${response}`);
+
+  }
+}
