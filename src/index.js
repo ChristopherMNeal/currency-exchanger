c859198a0528fe766161f86de22dfdc93e7235aa
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 12:41:32 -0800

    BUGFIX: Fix currency menu, Sets don't work with sub-arrays

--- src/index.js
+++ src/index.js
@@ -2,22 +2,51 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import ExchangeRateService from './js/exchange-rate-service';
+import ExchangeRateService from './js/exchange-rate-service';
 import CodesService from './js/codes-service';
 
-// function getElements(response) {
-//   if(response.result === "success") {
-//     $("#output-form").text(convertFunction(response));
-//   } else {
-//     $(".showErrors").text(`An error occured: ${response}`);
-//   }
-// }
+function getElements(response) {
+  if(response.result === "success") {
+    $("#output-form").text((response));
+  } else {
+    $(".showErrors").text(`An error occured: ${response}`);
+  }
+}
 
-// async function makeApiCall(baseCurrency) {
-//   const response = await ExchangeRateService.getRate(baseCurrency);
-//   getElements(response);
-// }
+async function makeApiCall(baseCurrency) {
+  const response = await ExchangeRateService.getRate(baseCurrency);
+  getElements(response);
+}
 
+async function getMenu() {
+  const response = await CodesService.getCodes();
+  if (response.result === "success") {
+    menuLoop(response);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+function menuLoop(response) {
+  for (let i = 0; i < response.supported_codes.length; i++) {
+    let currencyName = (response.supported_codes[i][1]);
+    let key = (response.supported_codes[i][0]);
+    let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
+    console.log(menuItem);
+    $("#base-currencies").append(menuItem);
+  }
+}
+
+
+getMenu();
+
+$("#base-currency-submit").click(function() {
+  let baseCurrency = $("#baseCurrencyMenu").val();
+  makeApiCall(baseCurrency);
+});
+
+// SETS DON'T WORK LIKE THIS AND I WASTED 2 HOURS TRYING TO MAKE THEM DO IT!
+/*
 async function getMenu() {
   const response = await CodesService.getCodes();
   if (response.result === "success") {
@@ -30,27 +59,23 @@ async function getMenu() {
 }
 
 function menuSetLoop(response) {
+
   let menuSet = new Set();
-  const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","Euro"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
+  const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","EUR"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
+  for (let i = 0; i < mostCommonCurrencies.length; i++) {
+    let currencyName = (mostCommonCurrencies[i][1]);
+    let key = (mostCommonCurrencies[i][0]);
+    menuSet.add([key, `${currencyName} (${key})`]);
+  }
   for (let i = 0; i < response.supported_codes.length; i++) {
     let currencyName = (response.supported_codes[i][1]);
     let key = (response.supported_codes[i][0]);
-    mostCommonCurrencies.push([key, currencyName]);
-  }
-  // console.log(mostCommonCurrencies);
-  for (let i = 0; i < mostCommonCurrencies.length; i++) {
-    // console.log(i);
-    let currencyName = (mostCommonCurrencies[i][1]);
-    let key = (mostCommonCurrencies[i][0]);
-    // console.log(`working: ${currencyName} (${key})`);
     menuSet.add([key, `${currencyName} (${key})`]);
   }
-  console.log(menuSet);
   return menuSet;
 }
 
 function menuLoop(menuSet) {
-  menuSetLoop(menuSet);
   menuSet.forEach((key) => {
     const name = key[1];
     const value = key[0];
@@ -59,9 +84,4 @@ function menuLoop(menuSet) {
   });
 }
 
-getMenu();
-
-// $("#base-currency-submit").click(function() {
-//   let baseCurrency = $("#baseCurrencyMenu").val();
-//   makeApiCall(baseCurrency);
-// });
+*/
