64b8f57efdb19abe615522d655db8c93bf15e681
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-19 12:54:01 -0800

    Finish form UI in index.html and index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import CodesService from './js/codes-service';
 
 function getElements(response) {
   if(response.result === "success") {
-    $("#output-form").text((response));
+    $("#output-form").text(calculateExchange(response));
   } else {
     $(".showErrors").text(`An error occured: ${response}`);
   }
@@ -28,12 +28,20 @@ async function getMenu() {
 }
 
 function menuLoop(response) {
+  const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","EUR"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"]];
+  for (let i = 0; i < mostCommonCurrencies.length; i++) {
+    let currencyName = (mostCommonCurrencies[i][1]);
+    let key = (response.supported_codes[i][0]);
+    let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
+    $("#common-base-currencies").append(menuItem);
+    $("#common-output-currencies").append(menuItem);
+  }
   for (let i = 0; i < response.supported_codes.length; i++) {
     let currencyName = (response.supported_codes[i][1]);
     let key = (response.supported_codes[i][0]);
     let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
-    console.log(menuItem);
-    $("#base-currencies").append(menuItem);
+    $("#all-base-currencies").append(menuItem);
+    $("#all-output-currencies").append(menuItem);
   }
 }
 
