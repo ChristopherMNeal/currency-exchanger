f9f17bbec4a972771f1b25020794320f1eada53b
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-22 07:04:17 -0800

    DRY menuLoop

--- src/index.js
+++ src/index.js
@@ -31,23 +31,21 @@ async function getMenu() {
   }
 }
 
+function menuFillLoop(menuInfo, scope) {
+  for (let i = 0; i < menuInfo.length; i++) {
+    let currencyName = (menuInfo[i][1]);
+    let key = (menuInfo[i][0]);
+    let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
+    $("#" + scope+ "-base-currencies").append(menuItem);
+    $("#" + scope+ "-output-currencies").append(menuItem);
+  }
+}
+
 function menuLoop(response) {
   const mostCommonCurrencies = [["USD","United States Dollar"],["EUR","EUR"],["JPY","Japanese Yen"],["GBP","Pound Sterling"],["AUD","Australian Dollar"],["CAD","Canadian Dollar"],["CHF","Swiss Franc"],["CNY","Chinese Renminbi"],["MXN","Mexican Peso"],["NZD", "New Zealand Dollar"],["SGD", "Singapore Dollar"],["SEK","Swedish Krona"],["KRW","South Korean Won"],["TRY","Turkish Lira"],["INR","Indian Rupee"],["BRL","Brazilian Real"],["ZAR","South African Rand"],["DKK","Danish Krone"],["TWD","New Taiwan Dollar"],["MYR","Malaysian Ringgit"],["XYZ", "FakeCoin"]];
-  for (let i = 0; i < mostCommonCurrencies.length; i++) {
-    let currencyName = (mostCommonCurrencies[i][1]);
-    let key = (mostCommonCurrencies[i][0]);
-    let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
-    $("#common-base-currencies").append(menuItem);
-    $("#common-output-currencies").append(menuItem);
-  }
-  for (let i = 0; i < response.supported_codes.length; i++) {
-    let currencyName = (response.supported_codes[i][1]);
-    let key = (response.supported_codes[i][0]);
-    let menuItem = `<option value="${key}">${currencyName} (${key})</option>`;
-    sessionStorage.setItem(key, currencyName);
-    $("#all-base-currencies").append(menuItem);
-    $("#all-output-currencies").append(menuItem);
-  }
+  const allCurrencies = response.supported_codes;
+  menuFillLoop(mostCommonCurrencies, "common");
+  menuFillLoop(allCurrencies, "all");
 }
 
 getMenu();
